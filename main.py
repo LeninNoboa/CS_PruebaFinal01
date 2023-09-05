bbeb287a50467bd1106fbf57e51ff24a7d90727d
lnoboa <slenov21@hotmail.com>
2023-09-05 16:27:48 -0500

    Commit

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from selenium.webdriver.common.by import By
 
 
 driver = webdriver.Chrome()
-driver.get("https://ecuador.patiotuerca.com/")
+driver.get("https://www.artefacta.com/")
 search_box = driver.find_element(by=By.CSS_SELECTOR, value="#search-list")
 search_box.send_keys("Audi A4")
 
 search_button = driver.find_element(by=By.CSS_SELECTOR, value="#openSearch > div > div > div.search-ots.false > img")
 search_button.click()
+
 
