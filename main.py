1a8eef4e84bf4c7df305e4ae4dfcdf625cd54c7a
lnoboa <slenov21@hotmail.com>
2023-09-05 16:02:07 -0500

    Commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+driver = webdriver.Chrome()
+driver.get("https://ecuador.patiotuerca.com/")
+search_box = driver.find_element(by=By.CSS_SELECTOR, value="#search-list")
+search_box.send_keys("Audi A4")
+
+search_button = driver.find_element(by=By.CSS_SELECTOR, value="#openSearch > div > div > div.search-ots.false > img")
+search_button.click()
+
